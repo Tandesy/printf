0d875e28b8f59b225b103497a5426414deb524f5
Tandesy <107643010+Tandesy@users.noreply.github.com>
2022-08-09 22:43:41 +0300

    printf project

--- get_width.c
+++ get_width.c
@@ -1,33 +1,32 @@
-#incluse "main.h"
- /**
-*get_width - Calculates the width for printing
-*@format: Formatted string in which to print the arguments
-*@i: list of arguments to be printed.
-*@list: list of arguments.
-*Return: width.
-*/
-int get_width(const char *format, int *i, va_list list)
-{
-int curr_i;
-int width = 0;
+#include "main.h"
 
-for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+/**
+ * get_flag - turns on flags if _printf finds
+ * a flag modifier in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ *
+ * Return: 1 if a flag has been turned on, 0 otherwise
+ */
+int get_flag(char s, flags_t *f)
 {
-if (is_digit(format[curr_i]))
-{
-width *= 10;
-width += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-width = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i 1;
+	int i = 0;
 
-return (width);
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+
+	return (i);
 }
